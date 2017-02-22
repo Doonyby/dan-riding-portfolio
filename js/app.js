4727d5cee00284f8efd753cfa78e85dd0c38a0fb
Dan Riding <doonyby@gmail.com>
2017-02-21 22:01:36 -0700

    added icons for links and divs for projects

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$('.introduction').show();
 	$('.body').hide();
+	$('#skiDiv, #elevationDiv, #bookDiv').hide();
 	$('#enter').click(function() {
 		$('.introduction').fadeOut("slow", "swing", function() {
 			$('.body').fadeIn(1000, "swing");
@@ -10,6 +11,12 @@ $(document).ready(function() {
 		$('.body').fadeOut("slow", "swing", function() {
 			$('.introduction').fadeIn(1000, "swing");
 		});
+	});
+	$('#ski, #elevation, #book').hover(function() {
+		$(this).css('opacity', '0.2');
+		$('#skiDiv').animate({marginLeft: "100px"}, 1000, "swing");
+	}, function() {
+		$(this).css('opacity', '1');
 	});
 
 });
