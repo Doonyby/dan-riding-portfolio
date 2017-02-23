260e0478d6d33c91a339d212d857f9733f1111b3
Dan Riding <doonyby@gmail.com>
2017-02-23 16:43:29 -0700

    remade design of site and put everything on a single page

--- js/app.js
+++ js/app.js
@@ -1,22 +1,9 @@
 $(document).ready(function() {
-	$('.introduction').show();
-	$('.body').hide();
-	$('#skiDiv, #elevationDiv, #bookDiv').hide();
 	$('#enter').click(function() {
-		$('.introduction').fadeOut("slow", "swing", function() {
-			$('.body').fadeIn(1000, "swing");
-		});
-	});
-	$('#backToCover').click(function() {
-		$('.body').fadeOut("slow", "swing", function() {
-			$('.introduction').fadeIn(1000, "swing");
-		});
-	});
-	$('#ski, #elevation, #book').hover(function() {
-		$(this).css('opacity', '0.2');
-		$('#skiDiv').animate({marginLeft: "100px"}, 1000, "swing");
-	}, function() {
-		$(this).css('opacity', '1');
+		$('.intro').animate({scrollTop: 775},800);
 	});
 
+	$('#backToTop').click(function() {
+		$('.intro').animate({scrollTop: 0},800);
+	});
 });
