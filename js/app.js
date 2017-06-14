dc33152b259b91a287c9ac2fe2a2a84bf9217f9b
Dan Riding <doonyby@gmail.com>
2017-06-14 12:31:10 -0600

    added hero animation, not working yet

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+	$("#landingName").animate({marginLeft: '0px'}, 1500);
+	$("#landingTitle").animate({marginLeft: '0px'}, 2000);
+	$("#landingCity").animate({marginLeft: '0px'}, 2500);
 	$('#profile').click(function() {
 		$(".aboutMe").velocity("scroll", 800);
 	});
